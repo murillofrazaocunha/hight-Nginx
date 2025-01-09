952fcaaee9d2d7ae9f7910705e83d79e90ed0bd8
Murillo Frazão Cunha <105169407+murillofrazaocunha@users.noreply.github.com>
2025-01-09 13:29:20 -0300

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-# [Cleanup] Remove all files in the temporary directory
-rm -rf /home/container/tmp/*
-
 # [Setup] Load the PHP version from the file
-PHP_VERSION=$(cat "/home/container/php_version.txt")
+PHP_VERSION=$(cat "/app/php_version.txt")
 
 # [Docker] Starting PHP-FPM with the specified PHP version
 echo "[Docker] Starting PHP-FPM"
-php-fpm$PHP_VERSION -c /home/container/php/php.ini --fpm-config /home/container/php/php-fpm.conf --daemonize > /dev/null 2>&1
+php-fpm$PHP_VERSION -c /app/php/php.ini --fpm-config /app/php/php-fpm.conf --daemonize > /dev/null 2>&1
 
 # [Docker] Starting NGINX
 echo "[Docker] Starting NGINX"
 echo "[Docker] Services successfully launched"
 
-nginx -c /home/container/nginx/nginx.conf -p /home/container
+nginx -c /app/nginx/nginx.conf -p /home/container
