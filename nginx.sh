0bb892594dd2257e19d379f9944ffffad40f7dcc
Murillo Frazão Cunha <105169407+murillofrazaocunha@users.noreply.github.com>
2025-01-09 14:29:16 -0300

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-
 # [Setup] Load the PHP version from the file
 PHP_VERSION=$(cat "/app/php_version.txt")
 
 # [Docker] Starting PHP-FPM with the specified PHP version
 echo "[Docker] Starting PHP-FPM"
-php-fpm$PHP_VERSION -c /app/php/php.ini --fpm-config /app/php/php-fpm.conf --daemonize > /dev/null 2>&1
+php-fpm$PHP_VERSION -c /app/php/php.ini --fpm-config /app/php/php-fpm.conf --daemonize
+
+# [Docker] Adjust socket permissions for Nginx to access it
+echo "[Docker] Adjusting permissions for PHP-FPM socket"
+chmod 770 /app/tmp/php-fpm.sock
+chown nobody:nogroup /app/tmp/php-fpm.sock
+
 
 # [Docker] Starting NGINX
 echo "[Docker] Starting NGINX"
